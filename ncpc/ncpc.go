ab9ea85201a44713188150a99bfbd0729edf2579
Sudarsan <sudarsan.sivakumar@paroscale.com>
2022-02-09 11:48:46 +0000

    Changed the name of modules and updated the clientAPI unwanted log fixes!

---
+++ ncpc/ncpc.go
@@ -0,0 +1,250 @@
+package main
+
+import (
+	"bytes"
+	"common/clientAPI"
+	"common/requestResponseLib"
+	"encoding/gob"
+	"encoding/json"
+	"flag"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"io/ioutil"
+	PumiceDBCommon "niova/go-pumicedb-lib/common"
+	"os"
+	"strings"
+	"time"
+)
+
+type clientHandler struct {
+	requestKey        string
+	requestValue      string
+	addr              string
+	port              string
+	operation         string
+	configPath        string
+	logPath           string
+	resultFile        string
+	operationMetaObjs []opData //For filling json data
+	clientAPIObj      clientAPI.ClientAPIHandler
+}
+
+type request struct {
+	Opcode    string    `json:"Operation"`
+	Key       string    `json:"Key"`
+	Value     string    `json:"Value"`
+	Timestamp time.Time `json:"Request_timestamp"`
+}
+
+type response struct {
+	Status        int       `json:"Status"`
+	ResponseValue string    `json:"Response"`
+	validate      bool      `json:"validate"`
+	Timestamp     time.Time `json:"Response_timestamp"`
+}
+type opData struct {
+	RequestData  request       `json:"Request"`
+	ResponseData response      `json:"Response"`
+	TimeDuration time.Duration `json:"Req_resolved_time"`
+}
+
+func usage() {
+	flag.PrintDefaults()
+	os.Exit(0)
+}
+
+//Function to get command line parameters
+func (handler *clientHandler) getCmdParams() {
+	flag.StringVar(&handler.requestKey, "k", "Key", "Key")
+	flag.StringVar(&handler.addr, "a", "127.0.0.1", "Addr value")
+	flag.StringVar(&handler.port, "p", "1999", "Port value")
+	flag.StringVar(&handler.requestValue, "v", "NULL", "Value")
+	flag.StringVar(&handler.configPath, "c", "./gossipNodes", "gossip nodes file path")
+	flag.StringVar(&handler.logPath, "l", "/tmp/temp.log", "Log path")
+	flag.StringVar(&handler.operation, "o", "NULL", "Specify the opeation to perform")
+	flag.StringVar(&handler.resultFile, "r", "operation", "Path along with file name for the result file")
+	flag.Parse()
+}
+
+//Write to Json
+func (cli *clientHandler) write2Json(toJson map[string][]opData) {
+	file, _ := json.MarshalIndent(toJson, "", " ")
+	_ = ioutil.WriteFile(cli.resultFile+".json", file, 0644)
+}
+
+func main() {
+	//Intialize client object
+	clientObj := clientHandler{}
+
+	//Get commandline parameters.
+	clientObj.getCmdParams()
+	flag.Usage = usage
+	if flag.NFlag() == 0 {
+		usage()
+		os.Exit(-1)
+	}
+
+	//Create logger
+	err := PumiceDBCommon.InitLogger(clientObj.logPath)
+	if err != nil {
+		log.Error("Error while initializing the logger  ", err)
+	}
+
+	log.Info("----START OF EXECUTION---")
+
+	//Init niovakv client API
+	clientObj.clientAPIObj = clientAPI.ClientAPIHandler{
+		Timeout: 10,
+	}
+	stop := make(chan int)
+	go func() {
+		err := clientObj.clientAPIObj.Start_ClientAPI(stop, clientObj.configPath)
+		if err != nil {
+			log.Error(err)
+			os.Exit(1)
+		}
+	}()
+	clientObj.clientAPIObj.Till_ready()
+
+	//Send request
+	var write bool
+	requestObj := requestResponseLib.KVRequest{}
+	responseObj := requestResponseLib.KVResponse{}
+
+	//Decl and init required variables
+	toJson := make(map[string][]opData)
+	value := make(map[string]string)
+	value["IP_ADDR"] = clientObj.addr
+	value["Port"] = clientObj.port
+	valueByte, _ := json.Marshal(value)
+	switch clientObj.operation {
+	case "write":
+		if clientObj.requestValue != "NULL" {
+			requestObj.Value = []byte(clientObj.requestValue)
+		} else {
+			requestObj.Value = valueByte
+		}
+		write = true
+		fallthrough
+
+	case "read":
+		requestObj.Key = clientObj.requestKey
+		requestObj.Operation = clientObj.operation
+		var requestByte bytes.Buffer
+		enc := gob.NewEncoder(&requestByte)
+		enc.Encode(requestObj)
+		//Send the write
+		responseBytes := clientObj.clientAPIObj.Request(requestByte.Bytes(), "", write)
+		dec := gob.NewDecoder(bytes.NewBuffer(responseBytes))
+		err = dec.Decode(&responseObj)
+		fmt.Println("Response:", string(responseObj.Value))
+
+		//Creation of output json
+		sendTime := time.Now()
+		requestMeta := request{
+			Opcode:    requestObj.Operation,
+			Key:       requestObj.Key,
+			Value:     string(responseObj.Value),
+			Timestamp: sendTime,
+		}
+
+		responseMeta := response{
+			Timestamp:     time.Now(),
+			Status:        responseObj.Status,
+			ResponseValue: string(responseObj.Value),
+		}
+
+		operationObj := opData{
+			RequestData:  requestMeta,
+			ResponseData: responseMeta,
+			TimeDuration: responseMeta.Timestamp.Sub(requestMeta.Timestamp),
+		}
+
+		clientObj.operationMetaObjs = append(clientObj.operationMetaObjs, operationObj)
+		if write {
+			toJson["write"] = clientObj.operationMetaObjs
+		} else {
+			toJson["read"] = clientObj.operationMetaObjs
+		}
+		clientObj.write2Json(toJson)
+
+	case "config":
+		responseBytes, err := clientObj.clientAPIObj.Get_PMDBServer_Config()
+		log.Info("Response : ", string(responseBytes))
+		if err != nil {
+			log.Error("Unable to get the config data")
+		}
+		_ = ioutil.WriteFile(clientObj.resultFile+".json", responseBytes, 0644)
+
+	case "membership":
+		toJson := clientObj.clientAPIObj.Get_Membership()
+		file, _ := json.MarshalIndent(toJson, "", " ")
+		_ = ioutil.WriteFile(clientObj.resultFile+".json", file, 0644)
+
+	case "general":
+		fmt.Printf("\033[2J")
+		fmt.Printf("\033[2;0H")
+		fmt.Print("UUID")
+		fmt.Printf("\033[2;38H")
+		fmt.Print("Type")
+		fmt.Printf("\033[2;50H")
+		fmt.Println("Status")
+		offset := 3
+		for {
+			lineCounter := 0
+			data := clientObj.clientAPIObj.Get_Membership()
+			for _, node := range data {
+				currentLine := offset + lineCounter
+				fmt.Print(node.Name)
+				fmt.Printf("\033[%d;38H", currentLine)
+				fmt.Print(node.Tags["Type"])
+				fmt.Printf("\033[%d;50H", currentLine)
+				fmt.Println(node.Status)
+				lineCounter += 1
+			}
+			time.Sleep(2 * time.Second)
+			fmt.Printf("\033[3;0H")
+			for i := 0; i < lineCounter; i++ {
+				fmt.Println("                                                       ")
+			}
+			fmt.Printf("\033[3;0H")
+		}
+	case "nisd":
+		fmt.Printf("\033[2J")
+		fmt.Printf("\033[2;0H")
+		fmt.Println("NISD_UUID")
+		fmt.Printf("\033[2;38H")
+		fmt.Print("Status")
+		fmt.Printf("\033[2;45H")
+		fmt.Println("Parent_UUID(Lookout)")
+		offset := 3
+		for {
+			lineCounter := 0
+			data := clientObj.clientAPIObj.Get_Membership()
+			for _, node := range data {
+				if (node.Tags["Type"] == "LOOKOUT") && (node.Status == "alive") {
+					for uuid, value := range node.Tags {
+						if uuid != "Type" {
+							currentLine := offset + lineCounter
+							fmt.Print(uuid)
+							fmt.Printf("\033[%d;38H", currentLine)
+							fmt.Print(strings.Split(value, "_")[0])
+							fmt.Printf("\033[%d;45H", currentLine)
+							fmt.Println(node.Name)
+							lineCounter += 1
+						}
+					}
+				}
+			}
+			time.Sleep(2 * time.Second)
+			fmt.Printf("\033[3;0H")
+			for i := 0; i < lineCounter; i++ {
+				fmt.Println("                                                       ")
+			}
+			fmt.Printf("\033[3;0H")
+		}
+	}
+
+	//clientObj.clientAPIObj.DumpIntoJson("./execution_summary.json")
+
+}
---
+++ proxy/proxy.go
@@ -0,0 +1,378 @@
+package main
+
+import (
+	"bufio"
+	"common/httpServer"
+	"encoding/json"
+	"errors"
+	"flag"
+	uuid "github.com/satori/go.uuid"
+	log "github.com/sirupsen/logrus"
+	"io/ioutil"
+	defaultLogger "log"
+	pmdbClient "niova/go-pumicedb-lib/client"
+	"niova/go-pumicedb-lib/common"
+	//"common/pmdbClient"
+	"common/serfAgent"
+	"os"
+	"os/signal"
+	"strconv"
+	"strings"
+	"syscall"
+	"time"
+)
+
+type proxyHandler struct {
+	//Other
+	configPath string
+	logLevel   string
+
+	//Niovakvserver
+	addr string
+
+	//Pmdb nivoa client
+	raftUUID                string
+	clientUUID              string
+	logPath                 string
+	PMDBServerConfigArray   []PeerConfigData
+	PMDBServerConfigByteMap map[string][]byte
+	pmdbClientObj           *pmdbClient.PmdbClientObj
+
+	//Serf agent
+	serfAgentName     string
+	serfAgentPort     string
+	serfAgentRPCPort  string
+	serfPeersFilePath string
+	serfLogger        string
+	serfAgentObj      serfAgent.SerfAgentHandler
+
+	//Http
+	httpPort      string
+	limit         string
+	requireStat   string
+	httpServerObj httpServer.HTTPServerHandler
+}
+
+type PeerConfigData struct {
+	PeerUUID   string
+	ClientPort string
+	Port       string
+	IPAddr     string
+}
+
+func usage() {
+	flag.PrintDefaults()
+	os.Exit(0)
+}
+
+//Function to get command line parameters while starting of the client.
+func (handler *proxyHandler) get_CmdParams() {
+	//Prepare default logpath
+	defaultLogPath := "/" + "tmp" + "/" + "niovaKVServer" + ".log"
+	flag.StringVar(&handler.raftUUID, "r", "NULL", "raft uuid")
+	flag.StringVar(&handler.clientUUID, "u", uuid.NewV4().String(), "client uuid")
+	flag.StringVar(&handler.logPath, "l", defaultLogPath, "log filepath")
+	flag.StringVar(&handler.configPath, "c", "./", "serf config path")
+	flag.StringVar(&handler.serfAgentName, "n", "NULL", "serf agent name")
+	flag.StringVar(&handler.limit, "e", "500", "No of concurrent request")
+	flag.StringVar(&handler.serfLogger, "sl", "ignore", "serf logger file [default:ignore]")
+	flag.StringVar(&handler.logLevel, "ll", "", "Set log level for the execution")
+	flag.StringVar(&handler.requireStat, "s", "0", "If required server stat about request enter 1")
+	flag.StringVar(&handler.serfPeersFilePath, "pa", "NULL", "Path to pmdb server gossip addrs")
+	flag.Parse()
+}
+
+/*
+Config should contain following:
+Name, Addr, Aport, Rport, Hport
+
+Name //For serf agent name, must be unique for each node
+Addr //Addr for serf agent and http listening
+Aport //Serf agent-agent communication
+Rport //Serf agent-client communication
+Hport //Http listener port
+*/
+func (handler *proxyHandler) get_ConfigData() error {
+	reader, err := os.Open(handler.configPath)
+	if err != nil {
+		return err
+	}
+	filescanner := bufio.NewScanner(reader)
+	filescanner.Split(bufio.ScanLines)
+	for filescanner.Scan() {
+		input := strings.Split(filescanner.Text(), " ")
+		if input[0] == handler.serfAgentName {
+			handler.addr = input[1]
+			handler.serfAgentPort = input[2]
+			handler.serfAgentRPCPort = input[3]
+			handler.httpPort = input[4]
+		}
+	}
+	if handler.serfAgentPort == "" {
+		return errors.New("Agent name not matching or not provided")
+	}
+	return nil
+}
+
+//start the Niovakvpmdbclient
+func (handler *proxyHandler) start_PMDBClient() error {
+	var err error
+
+	//Get client object.
+	handler.pmdbClientObj = pmdbClient.PmdbClientNew(handler.raftUUID, handler.clientUUID)
+	if handler.pmdbClientObj == nil {
+		return errors.New("PMDB client object is empty")
+	}
+
+	//Start pumicedb client.
+	err = handler.pmdbClientObj.Start()
+	if err != nil {
+		return err
+	}
+
+	//Store rncui in nkvclientObj.i
+	handler.pmdbClientObj.AppUUID = uuid.NewV4().String()
+	return nil
+
+}
+
+//start the SerfAgentHandler
+func (handler *proxyHandler) start_SerfAgent() error {
+	switch handler.serfLogger {
+	case "ignore":
+		defaultLogger.SetOutput(ioutil.Discard)
+	default:
+		f, err := os.OpenFile(handler.serfLogger, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+		if err != nil {
+			defaultLogger.SetOutput(os.Stderr)
+		} else {
+			defaultLogger.SetOutput(f)
+		}
+	}
+
+	handler.serfAgentObj = serfAgent.SerfAgentHandler{}
+	handler.serfAgentObj.Name = handler.serfAgentName
+	handler.serfAgentObj.BindAddr = handler.addr
+	handler.serfAgentObj.BindPort = handler.serfAgentPort
+	handler.serfAgentObj.AgentLogger = defaultLogger.Default()
+	handler.serfAgentObj.RpcAddr = handler.addr
+	handler.serfAgentObj.RpcPort = handler.serfAgentRPCPort
+	joinAddrs, err := serfAgent.Get_PeerAddress(handler.serfPeersFilePath)
+	if err != nil {
+		return err
+	}
+	//Start serf agent
+	_, err = handler.serfAgentObj.Serf_agent_startup(joinAddrs, true)
+
+	return err
+}
+
+func (handler *proxyHandler) get_PMDBServer_Config() {
+	var raftUUID, peerConfig string
+	for raftUUID == "" {
+		peerConfig, raftUUID = handler.serfAgentObj.Get_tags()
+		time.Sleep(2 * time.Second)
+	}
+	log.Info("PMDB config recvd from gossip : ", peerConfig)
+	handler.raftUUID = raftUUID
+	handler.PMDBServerConfigByteMap = make(map[string][]byte)
+
+	splitData := strings.Split(peerConfig, "/")
+	var PeerUUID, ClientPort, Port, IPAddr string
+	flag := false
+	for i, element := range splitData {
+		switch i % 4 {
+		case 0:
+			PeerUUID = element
+		case 1:
+			IPAddr = element
+		case 2:
+			ClientPort = element
+		case 3:
+			flag = true
+			Port = element
+		}
+		if flag {
+			peerConfig := PeerConfigData{
+				PeerUUID:   PeerUUID,
+				IPAddr:     IPAddr,
+				Port:       Port,
+				ClientPort: ClientPort,
+			}
+			handler.PMDBServerConfigArray = append(handler.PMDBServerConfigArray, peerConfig)
+			handler.PMDBServerConfigByteMap[PeerUUID], _ = json.Marshal(peerConfig)
+			flag = false
+		}
+	}
+	path := os.Getenv("NIOVA_LOCAL_CTL_SVC_DIR")
+	//os.Mkdir(path+"/"+handler.clientUUID,os.ModePerm)
+	//log.Info("Peer UUID : ",handler.clientUUID)
+	//path += "/" + handler.clientUUID
+	os.Mkdir(path, os.ModePerm)
+	//path += "/PMDBConfig/"
+	//log.Info("Altered NIOVA_LOCAL_CTL_SVC_DIR is ", path)
+	//os.Setenv("NIOVA_LOCAL_CTL_SVC_DIR",path)
+	handler.dump_ConfigToFile(path + "/")
+}
+
+func (handler *proxyHandler) dump_ConfigToFile(outfilepath string) {
+	//Generate .raft
+	raft_file, err := os.Create(outfilepath + handler.raftUUID + ".raft")
+	if err != nil {
+		log.Error(err)
+	}
+
+	_, errFile := raft_file.WriteString("RAFT " + handler.raftUUID + "\n")
+	if errFile != nil {
+		log.Error(errFile)
+	}
+
+	for _, peer := range handler.PMDBServerConfigArray {
+		raft_file.WriteString("PEER " + peer.PeerUUID + "\n")
+	}
+
+	raft_file.Sync()
+	raft_file.Close()
+
+	//Generate .peer
+	for _, peer := range handler.PMDBServerConfigArray {
+		peer_file, err := os.Create(outfilepath + peer.PeerUUID + ".peer")
+		if err != nil {
+			log.Error(err)
+		}
+
+		_, errFile := peer_file.WriteString(
+			"RAFT         " + handler.raftUUID +
+				"\nIPADDR       " + peer.IPAddr +
+				"\nPORT         " + peer.Port +
+				"\nCLIENT_PORT  " + peer.ClientPort +
+				"\nSTORE        ./*.raftdb\n")
+
+		if errFile != nil {
+			log.Error(errFile)
+		}
+		peer_file.Sync()
+		peer_file.Close()
+	}
+}
+
+func (handler *proxyHandler) start_HTTPServer() error {
+	//Start httpserver.
+	handler.httpServerObj = httpServer.HTTPServerHandler{}
+	handler.httpServerObj.Addr = handler.addr
+	handler.httpServerObj.Port = handler.httpPort
+	handler.httpServerObj.PUTHandler = handler.pmdbClientObj.WriteEncoded
+	handler.httpServerObj.GETHandler = handler.pmdbClientObj.ReadEncoded
+	handler.httpServerObj.HTTPConnectionLimit, _ = strconv.Atoi(handler.limit)
+	handler.httpServerObj.PMDBServerConfig = handler.PMDBServerConfigByteMap
+	if handler.requireStat != "0" {
+		handler.httpServerObj.StatsRequired = true
+	}
+	err := handler.httpServerObj.Start_HTTPServer()
+	return err
+}
+
+//Get gossip data
+func (handler *proxyHandler) set_Serf_GossipData() {
+	tag := make(map[string]string)
+	tag["Hport"] = handler.httpPort
+	tag["Aport"] = handler.serfAgentPort
+	tag["Rport"] = handler.serfAgentRPCPort
+	tag["Type"] = "PROXY"
+	handler.serfAgentObj.Set_node_tags(tag)
+	for {
+		leader, err := handler.pmdbClientObj.PmdbGetLeader()
+		if err != nil {
+			log.Error(err)
+			//Wait for sometime to pmdb client to establish connection with raft cluster or raft cluster to appoint a leader
+			time.Sleep(5 * time.Second)
+			continue
+		}
+		tag["Leader UUID"] = leader.String()
+		handler.serfAgentObj.Set_node_tags(tag)
+		log.Trace("(Proxy)", tag)
+		time.Sleep(300 * time.Millisecond)
+	}
+}
+
+func (handler *proxyHandler) killSignal_Handler() {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigs
+		json_data, _ := json.MarshalIndent(handler.httpServerObj.Stat, "", " ")
+		_ = ioutil.WriteFile(handler.clientUUID+".json", json_data, 0644)
+		log.Info("(Proxy) Received a kill signal")
+		os.Exit(1)
+	}()
+}
+
+//Main func
+func main() {
+
+	var err error
+
+	proxyObj := proxyHandler{}
+	//Get commandline paraameters.
+	proxyObj.get_CmdParams()
+
+	flag.Usage = usage
+	flag.Parse()
+	if flag.NFlag() == 0 {
+		usage()
+		os.Exit(-1)
+	}
+	//niovaServer.clientUUID = uuid.NewV4().String()
+	//Create log file
+	err = PumiceDBCommon.InitLogger(proxyObj.logPath)
+	switch proxyObj.logLevel {
+	case "Info":
+		log.SetLevel(log.InfoLevel)
+	case "Trace":
+		log.SetLevel(log.TraceLevel)
+	}
+	if err != nil {
+		log.Error("(Proxy) Logger error : ", err)
+	}
+
+	//get config data
+	err = proxyObj.get_ConfigData()
+	if err != nil {
+		log.Error("(Proxy) Error while getting config data : ", err)
+		os.Exit(1)
+	}
+
+	//Start serf agent handler
+	err = proxyObj.start_SerfAgent()
+	if err != nil {
+		log.Error("Error while starting serf agent : ", err)
+		os.Exit(1)
+	}
+
+	//Get PMDB server config data
+	proxyObj.get_PMDBServer_Config()
+
+	//Create a niovaKVServerHandler
+	err = proxyObj.start_PMDBClient()
+	if err != nil {
+		log.Error("(Niovakv Server) Error while starting pmdb client : ", err)
+		os.Exit(1)
+	}
+
+	//Start http server
+	go func() {
+		log.Info("Starting HTTP server")
+		err = proxyObj.start_HTTPServer()
+		if err != nil {
+			log.Error("Error while starting http server : ", err)
+		}
+	}()
+
+	//Stat maker
+	if proxyObj.requireStat != "0" {
+		go proxyObj.killSignal_Handler()
+	}
+
+	//Start the gossip
+	proxyObj.set_Serf_GossipData()
+}
