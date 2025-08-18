e375b781112295a57336e96c97d602b9ffee11a2
bhanu <bhanu.praneeth@paroscale.com>
2025-08-18 04:34:46 +0000

    fix(ccManager): add support for multiple nisd
    	- added support for multiple nisd's, the block devices
    	  names can be passed as comma separated values to the
    	  -nd parameter and then the ccManager queries each block
    	  device and updates the config with all nisd details.

--- controlplane/containerConfigManager/ccManager.go
+++ controlplane/containerConfigManager/ccManager.go
@@ -6,51 +6,60 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
 
 	log "github.com/sirupsen/logrus"
 	yaml "gopkg.in/yaml.v3"
 
 	cpClient "github.com/00pauln00/niova-mdsvc/controlplane/ctlplanefuncs/client"
-	cpLib "github.com/00pauln00/niova-mdsvc/controlplane/ctlplanefuncs/lib"
 
 	"github.com/google/uuid"
 )
 
-type ContainerConfig struct {
-	Nisd_Config []cpLib.Nisd `yaml: "nisd_config"`
+type s3Config struct {
+	URL  string `yaml: "url"`
+	Opts string `yaml: "opts"`
+	Auth string `yaml: "auth"`
 }
 
-func populateNisd(nisd map[string][]byte, UniqID string) cpLib.Nisd {
+type NisdConfig struct {
+	Name   string `yaml: "name"`
+	Nisd   string `yaml: "uuid"`
+	Client uint16 `yaml: "client_port"`
+	Peer   uint16 `yaml: "peer_port"`
+}
+
+type ContainerConfig struct {
+	S3Config   s3Config     `yaml: "s3_config"`
+	NisdConfig []NisdConfig `yaml: "nisd_config"`
+}
+
+func populateNisd(nisd map[string][]byte, UniqID string) NisdConfig {
 	cp, _ := strconv.ParseUint(string(nisd[fmt.Sprintf("/n/%s/conf_cp", UniqID)]), 10, 16)
 	pp, _ := strconv.ParseUint(string(nisd[fmt.Sprintf("/n/%s/conf_pp", UniqID)]), 10, 16)
-	return cpLib.Nisd{
-		DeviceID:   string(nisd[fmt.Sprintf("/n/%s/conf_d", UniqID)]),
-		DeviceUUID: uuid.MustParse(UniqID),
-		ClientPort: uint16(cp),
-		PeerPort:   uint16(pp),
+	return NisdConfig{
+		Name:   string(nisd[fmt.Sprintf("/n/%s/conf_d", UniqID)]),
+		Nisd:   UniqID,
+		Client: uint16(cp),
+		Peer:   uint16(pp),
 	}
 }
 
-func generateConfig(nisd map[string][]byte, UniqID, config string) error {
-	err := os.MkdirAll(filepath.Dir(config), os.ModePerm)
-	if err != nil {
-		log.Error("Error creating directories:", err)
+// LoadOrCreateConfig loads config from file if present, otherwise creates a new one.
+func LoadOrCreateConfig(setupConfig string, cc *ContainerConfig) error {
+	if err := os.MkdirAll(filepath.Dir(setupConfig), os.ModePerm); err != nil {
 		return err
-	}
-	cc := ContainerConfig{
-		Nisd_Config: []cpLib.Nisd{
-			populateNisd(nisd, UniqID),
-		},
 	}
 
-	configY, err := yaml.Marshal(cc)
-	if err != nil {
-		log.Error("Error marshaling YAML:", err)
-		return err
-	}
-	err = os.WriteFile(config, configY, 0644)
-	if err != nil {
-		log.Error("Error writing YAML file:", err)
+	if _, err := os.Stat(setupConfig); err == nil {
+		data, err := os.ReadFile(setupConfig)
+		if err != nil {
+			return err
+		}
+		if err := yaml.Unmarshal(data, cc); err != nil {
+			return err
+		}
+	} else if !os.IsNotExist(err) {
 		return err
 	}
 
@@ -60,21 +69,45 @@ func generateConfig(nisd map[string][]byte, UniqID, config string) error {
 func main() {
 
 	raftID := flag.String("r", "", "pass the raft uuid")
-	configPath := flag.String("c", "", "pass the gossip config path")
+	configPath := flag.String("c", "./gossipNodes", "pass the gossip config path")
 	setupConfig := flag.String("sc", "./config.yaml", "pass the gossip config path")
-	deviceID := flag.String("d", "", "pass the nisd device id")
+	deviceList := flag.String("d", "", "pass the nisd device id")
 	flag.Parse()
-	log.Infof("starting config app - raft: %s, config: %s, device id: %s", *raftID, *configPath, *deviceID)
+	deviceIDs := strings.Split(*deviceList, ",")
+	log.Infof("starting config app - raft: %s, config: %s, device id: %s", *raftID, *configPath, *deviceList)
+
 	c := cpClient.InitCliCFuncs(uuid.New().String(), *raftID, *configPath)
-	uid, err := c.GetDeviceUUID(*deviceID)
+	var cc ContainerConfig
+	err := LoadOrCreateConfig(*setupConfig, &cc)
 	if err != nil {
-		log.Error("failed to get device uuid", err)
+		log.Error("failed to load or create config file: ", err)
+		os.Exit(-1)
 	}
-	log.Info("device uuid: ", string(uid))
-	nisd, err := c.GetNisdDetails(string(uid))
+	cc.NisdConfig = make([]NisdConfig, 0)
+
+	for _, deviceID := range deviceIDs {
+		uid, err := c.GetDeviceUUID(deviceID)
+		if err != nil {
+			log.Error("failed to get device uuid", err)
+			os.Exit(-1)
+		}
+		log.Info("device uuid: ", string(uid))
+		nisd, err := c.GetNisdDetails(string(uid))
+		if err != nil {
+			log.Error("failed to get nisd details:", err)
+		}
+		cc.NisdConfig = append(cc.NisdConfig, populateNisd(nisd, string(uid)))
+	}
+
+	log.Info("config struct: ", cc)
+	configY, err := yaml.Marshal(cc)
 	if err != nil {
-		log.Error("failed to get nisd details:", err)
+		log.Error("Error marshaling YAML:", err)
+		os.Exit(-1)
 	}
-	log.Info("nisd details: ", nisd)
-	generateConfig(nisd, string(uid), *setupConfig)
+	err = os.WriteFile(*setupConfig, configY, 0644)
+	if err != nil {
+		log.Error("Error writing YAML file:", err)
+		os.Exit(-1)
+	}
 }
--- go.mod
+++ go.mod
@@ -14,7 +14,7 @@ require (
 	github.com/satori/go.uuid v1.2.0
 	github.com/sirupsen/logrus v1.9.3
 	golang.org/x/exp v0.0.0-20250506013437-ce4c2cf36ca6
-	gopkg.in/yaml.v2 v2.4.0
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
