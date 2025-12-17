689b5fd091d1140c22e7a4e02172904c00d9c851
bhanu <bhanu.praneeth@paroscale.com>
2025-12-17 05:42:21 +0000

    fix(CP): update nisd available space
    	- Nisd available space value updation in rocksdb

--- controlplane/ctlplanefuncs/server/hierarchy.go
+++ controlplane/ctlplanefuncs/server/hierarchy.go
@@ -166,7 +166,7 @@ func (hr *Hierarchy) UpdateNisdCopy(nisd *ctlplfl.Nisd) *ctlplfl.NisdCopy {
 }
 
 // Pick a  NISD using the hash from a specific failure domain.
-func (hr *Hierarchy) PickNISD(fd int, entityIDX int, hash uint64, picked map[string]struct{}) (*cpLib.Nisd, error) {
+func (hr *Hierarchy) PickNISD(fd int, entityIDX int, hash uint64, picked map[string]struct{}) (*cpLib.NisdCopy, error) {
 	if int(fd) >= len(hr.FD) {
 		log.Error("failed to get fd: ", fd)
 		return nil, errors.New("invalid fd tier")
@@ -206,7 +206,7 @@ func (hr *Hierarchy) PickNISD(fd int, entityIDX int, hash uint64, picked map[str
 			if _, exists := picked[nCopy.Ptr.ID]; !exists {
 				// commit selection
 				picked[nCopy.Ptr.ID] = struct{}{}
-				return nisd, nil
+				return nCopy, nil
 			}
 		}
 
