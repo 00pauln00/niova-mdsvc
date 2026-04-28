049564b9ff0e190995bd434dd292f8a8fe24068b
HimaniMalani17 <himani.malani@paroscale.com>
2026-04-28 07:39:04 +0000

    Corrected format by running gofmt -w

--- controlplane/pmdbServer/pmdbServer.go
+++ controlplane/pmdbServer/pmdbServer.go
@@ -627,9 +627,9 @@ func (nso *NiovaKVServer) Read(readArgs *PumiceDBServer.PmdbCbArgs) int64 {
 		})
 
 		if err != nil {
-        	log.Errorf("RangeRead failed: err=%v, readResult nil=%v", err, readResult == nil)
-        	return -1
-    	}
+			log.Errorf("RangeRead failed: err=%v, readResult nil=%v", err, readResult == nil)
+			return -1
+		}
 
 		var cRead bool
 		if readResult.LastKey != "" {
